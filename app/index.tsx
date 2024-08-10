266f18e15d7b95d2c210ed5c5bcc381174f393d8
eliaskounakas <elias.kounakas@proton.me>
2024-08-10 12:13:56 +0200

    use calendar library

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,8 @@
 import { Text, View } from "react-native";
+import { TimelineCalendar } from "@howljs/calendar-kit";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <TimelineCalendar viewMode="week" />
   );
 }
