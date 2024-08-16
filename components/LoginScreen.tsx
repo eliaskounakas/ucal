dacbf250e777191694d8fa25c943eb777c55151c
Elias Kounakas <eliaskounakas@proton.me>
2024-08-16 13:51:38 +0200

    add bouncy checkbox button (not functional yet(

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
-import { SafeAreaView } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+const { width: ScreenWidth } = Dimensions.get("screen");
 import LoginScreen from 'react-native-login-screen'
 import UspaceClient from 'uspace-api-wrapper';
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 
 export interface LoginProps {
   session: string,
@@ -11,11 +13,14 @@ export interface LoginProps {
 export default function Login({ session, setSession }: LoginProps) {
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [stayLoggedIn, setStayLoggedIn] = useState<boolean>(false);
 
   const uClient = new UspaceClient();
   
   return (
-      <LoginScreen
+      <>
+        <LoginScreen
+        style={styles.loginContainer}
         logoImageSource={require('../assets/icon.png')}
         onLoginPress={async () => {
          uClient.login(username, password)
@@ -32,9 +37,37 @@ export default function Login({ session, setSession }: LoginProps) {
         onPasswordChange={setPassword}
         enablePasswordValidation
         disableSignup
+        disableDivider
         disableSocialButtons
         emailPlaceholder={"Username"}
         disableEmailTooltip
+        textInputChildren={
+          <BouncyCheckbox
+            style={styles.checkbox}
+            size={25}
+            fillColor="#25A9E2"
+            unFillColor="#FFFFFF"
+            text="Stay signed in"
+            iconStyle={{ borderColor: "#25A9E2" }}
+            innerIconStyle={{ borderWidth: 2 }}
+            textStyle={{
+              textDecorationLine: "none",
+            }}
+            onPress={() => {}}
+          />
+        }
       />
+      </>
+ 
   )
 }
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    paddingTop: '50%',
+  },
+  checkbox: {
+    marginTop: 16,
+    width: ScreenWidth * 0.9,
+  },
+});
