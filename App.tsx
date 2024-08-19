0a4c60ef6e84e5a02a22a473016bad50178b78b6
Elias Kounakas <eliaskounakas@proton.me>
2024-08-19 13:25:19 +0200

    add Modal with course info

--- App.tsx
+++ App.tsx
@@ -1,100 +1,151 @@
-import React, { useState } from 'react';
-import { View, SafeAreaView, StyleSheet } from 'react-native';
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
-import { EventItem } from '@howljs/calendar-kit';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import Calendar from './components/Calendar';
-import Login from './components/LoginScreen';
+import React, { useState } from "react";
+import { View, SafeAreaView, StyleSheet } from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerItemList,
+} from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { EventItem } from "@howljs/calendar-kit";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Calendar from "./components/Calendar";
+import Login from "./components/LoginScreen";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   const [events, setEvents] = useState<EventItem[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [session, setSession] = useState<string>('');
+  const [session, setSession] = useState<string>("");
 
   const calendarProps = { session, events, setEvents, isLoading, setIsLoading };
   const loginProps = { setSession };
 
-
   function CustomCalendarDrawerContent(props: any) {
     return (
-      <DrawerContentScrollView {...props} contentContainerStyle={{flex: 1,  flexDirection: 'column', justifyContent: 'space-between' }}>
+      <DrawerContentScrollView
+        {...props}
+        contentContainerStyle={{
+          flex: 1,
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <SafeAreaView>
+          <DrawerItem
+            label="Calendar"
+            labelStyle={{ fontSize: 25, color: "black" }}
+            style={{ marginBottom: 5 }}
+            onPress={() => {}}
+          />
+          <DrawerItemList {...props} />
+          <View style={styles.drawerDivider} />
+        </SafeAreaView>
 
         <SafeAreaView>
-          <DrawerItem label="Calendar" labelStyle={{fontSize: 25, color: 'black'}} style={{marginBottom: 5}} onPress={() => {}}/>
-          <DrawerItemList {...props} />
-          <View style={styles.drawerDivider}/>
-        </SafeAreaView>
-        
-        <SafeAreaView>
-        <View style={styles.drawerDivider} />
-          <DrawerItem 
-            label="Logout" 
-            labelStyle={{color: '#cc0000'}}
-            onPress={() => {setSession('')}} 
-            icon={() => <MaterialIcons name="logout" size={24} color="#cc0000" />}
+          <View style={styles.drawerDivider} />
+          <DrawerItem
+            label="Logout"
+            labelStyle={{ color: "#cc0000" }}
+            onPress={() => {
+              setSession("");
+            }}
+            icon={() => (
+              <MaterialIcons name="logout" size={24} color="#cc0000" />
+            )}
           />
         </SafeAreaView>
-
       </DrawerContentScrollView>
     );
   }
-  
+
   if (session) {
     return (
       <NavigationContainer independent={true}>
-        <Drawer.Navigator 
+        <Drawer.Navigator
           initialRouteName="Work Week"
           screenOptions={{
-            title: 'Calendar',
+            title: "Calendar",
           }}
-          drawerContent={props => <CustomCalendarDrawerContent {...props}/>}
+          drawerContent={(props) => <CustomCalendarDrawerContent {...props} />}
         >
-          <Drawer.Screen name="Week" options={{
-            drawerLabel: 'Week',
-            drawerActiveTintColor: '#25A9E2',
-            drawerIcon: ({ focused }) => <MaterialCommunityIcons name="calendar-week" size={24} color={focused ? '#25A9E2' : 'grey'} />        
-          }}>
-            {() => <Calendar viewMode={'week'} {...calendarProps}/>}
-          </Drawer.Screen >
-          <Drawer.Screen name="Work Week" options={{
-            drawerLabel: 'Work Week',
-            drawerActiveTintColor: '#25A9E2',
-            drawerIcon: ({ focused }) => <MaterialIcons name="calendar-view-week" size={24} color={focused ? '#25A9E2' : 'grey'} />        
-          }}>
-            {() => <Calendar viewMode={'workWeek'} {...calendarProps}/>}
+          <Drawer.Screen
+            name="Week"
+            options={{
+              drawerLabel: "Week",
+              drawerActiveTintColor: "#25A9E2",
+              drawerIcon: ({ focused }) => (
+                <MaterialCommunityIcons
+                  name="calendar-week"
+                  size={24}
+                  color={focused ? "#25A9E2" : "grey"}
+                />
+              ),
+            }}
+          >
+            {() => <Calendar viewMode={"week"} {...calendarProps} />}
           </Drawer.Screen>
-          <Drawer.Screen name="3 Days" options={{
-            drawerLabel: '3 Days',
-            drawerActiveTintColor: '#25A9E2',
-            drawerIcon: ({ focused }) => <MaterialCommunityIcons name="view-week-outline" size={24} color={focused ? '#25A9E2' : 'grey'} />        
-          }}>
-            {() => <Calendar viewMode={'threeDays'} {...calendarProps}/>}
+          <Drawer.Screen
+            name="Work Week"
+            options={{
+              drawerLabel: "Work Week",
+              drawerActiveTintColor: "#25A9E2",
+              drawerIcon: ({ focused }) => (
+                <MaterialIcons
+                  name="calendar-view-week"
+                  size={24}
+                  color={focused ? "#25A9E2" : "grey"}
+                />
+              ),
+            }}
+          >
+            {() => <Calendar viewMode={"workWeek"} {...calendarProps} />}
           </Drawer.Screen>
-          <Drawer.Screen name="Day" options={{
-            drawerLabel: 'Day',
-            drawerActiveTintColor: '#25A9E2',
-            drawerIcon: ({ focused }) => <MaterialCommunityIcons name="view-day-outline" size={24} color={focused ? '#25A9E2' : 'grey'} />            
-          }}>
-            {() => <Calendar viewMode={'day'} {...calendarProps}/>}
+          <Drawer.Screen
+            name="3 Days"
+            options={{
+              drawerLabel: "3 Days",
+              drawerActiveTintColor: "#25A9E2",
+              drawerIcon: ({ focused }) => (
+                <MaterialCommunityIcons
+                  name="view-week-outline"
+                  size={24}
+                  color={focused ? "#25A9E2" : "grey"}
+                />
+              ),
+            }}
+          >
+            {() => <Calendar viewMode={"threeDays"} {...calendarProps} />}
+          </Drawer.Screen>
+          <Drawer.Screen
+            name="Day"
+            options={{
+              drawerLabel: "Day",
+              drawerActiveTintColor: "#25A9E2",
+              drawerIcon: ({ focused }) => (
+                <MaterialCommunityIcons
+                  name="view-day-outline"
+                  size={24}
+                  color={focused ? "#25A9E2" : "grey"}
+                />
+              ),
+            }}
+          >
+            {() => <Calendar viewMode={"day"} {...calendarProps} />}
           </Drawer.Screen>
         </Drawer.Navigator>
       </NavigationContainer>
     );
   }
-  
-  return (
-    <Login {...loginProps}/>
-  )
-}
 
+  return <Login {...loginProps} />;
+}
 
 const styles = StyleSheet.create({
   drawerDivider: {
-    borderBottomColor: 'grey',
+    borderBottomColor: "grey",
     borderBottomWidth: 1.5,
   },
-})
+});
--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,100 +1,167 @@
-import { useEffect, useState} from 'react'
-import { SafeAreaView, StyleSheet, Modal, View, Text, Pressable, Dimensions } from 'react-native';
+import { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Modal,
+  View,
+  Text,
+  Pressable,
+  Dimensions,
+} from "react-native";
 const { width: ScreenWidth } = Dimensions.get("screen");
-import { TimelineCalendar, EventItem, CalendarViewMode, PackedEvent } from '@howljs/calendar-kit';
-import UspaceClient from 'uspace-api-wrapper';
-import type { CourseData } from 'uspace-api-wrapper/dist/entities';
-import { StatusBar } from 'expo-status-bar';
-import Feather from '@expo/vector-icons/Feather';
+import {
+  TimelineCalendar,
+  EventItem,
+  CalendarViewMode,
+  PackedEvent,
+} from "@howljs/calendar-kit";
+import UspaceClient from "uspace-api-wrapper";
+import type { CourseData } from "uspace-api-wrapper/dist/entities";
+import { StatusBar } from "expo-status-bar";
+import Feather from "@expo/vector-icons/Feather";
+import MoodleLogo from "../assets/moodle.svg";
+import WebLink from "./WebLink";
 
 export interface CalendarProps {
-  viewMode: CalendarViewMode, 
-  session: string,
-  events: EventItem[], 
-  setEvents: Function, 
-  isLoading: boolean, 
-  setIsLoading: Function
+  viewMode: CalendarViewMode;
+  session: string;
+  events: EventItem[];
+  setEvents: Function;
+  isLoading: boolean;
+  setIsLoading: Function;
 }
 
 export interface CalendarModalProps {
-  title: string,
-  date: string,
-  startTime: string, 
-  endTime: string,
-  location: string,
+  title: string;
+  date: string;
+  startTime: string;
+  endTime: string;
+  location: string;
+  ufindLink: string;
+  moodleLink: string;
 }
 
-export default function Calendar({ viewMode, session, events, setEvents, isLoading, setIsLoading }: CalendarProps) {
+export default function Calendar(props: CalendarProps) {
   const [modalVisible, setModalVisible] = useState(false);
   const [modalProps, setModalProps] = useState<CalendarModalProps | null>(null);
 
   useEffect(() => {
-    //need to make this better understandable, isLoaded/fetchedCourses/isCached would make more sense 
-    if (isLoading) {
-      fetchCourses(session)
-      .then((res) => {
-        setEvents(() => [...res]);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    //need to make this better understandable, isLoaded/fetchedCourses/isCached would make more sense
+    if (props.isLoading) {
+      fetchCourses(props.session)
+        .then((res) => {
+          props.setEvents(() => [...res]);
+        })
+        .finally(() => {
+          props.setIsLoading(false);
+        });
     }
   }, []);
 
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <TimelineCalendar 
-          viewMode={viewMode}
-          events={events}
-          isLoading={isLoading}
-          initialDate='2024-03-01'
-          theme={{ loadingBarColor: '#0063A6' }}
-          onPressEvent={(event) => {setModalProps(generateModalProps(event)); setModalVisible(true); console.log(event)}}
+        <TimelineCalendar
+          viewMode={props.viewMode}
+          events={props.events}
+          isLoading={props.isLoading}
+          initialDate="2024-03-01"
+          theme={{ loadingBarColor: "#0063A6" }}
+          onPressEvent={(event) => {
+            setModalProps(generateModalProps(event));
+            setModalVisible(true);
+            console.log(event);
+          }}
         />
         <Modal
           transparent={true}
           visible={modalVisible}
           onRequestClose={() => {
             setModalVisible(!modalVisible);
-          }}>
-          <Pressable style={styles.modalContainer} onPress={() => setModalVisible(false)}>
-              <Pressable style={[styles.modalView, {marginBottom: 0}]} >
-                <Text style={styles.modalTitle}>{modalProps?.title}</Text>
-                <View style={styles.divider}/>
-                <View style={styles.modalRow}>
-                  <Feather name="clock" size={15} color="#ADD8E6" style={{alignSelf: 'center'}}/>
-                  <Text>{modalProps?.startTime}-{modalProps?.endTime}</Text>
-                  <Text>{modalProps?.date}</Text>
-                </View>
-                <View style={styles.modalRow}>
-                  <Feather name="map-pin" size={15} color="#ADD8E6" style={{alignSelf: 'center'}}/>
-                  <Text>{modalProps?.location}</Text>
-                </View>
-              </Pressable>
+          }}
+        >
+          <Pressable
+            style={styles.modalContainer}
+            onPress={() => setModalVisible(false)}
+          >
+            <Pressable style={[styles.modalView, { marginBottom: 0 }]}>
+              <Text style={styles.modalTitle}>{modalProps?.title}</Text>
+              <View style={styles.divider} />
+              <View style={styles.modalRow}>
+                <Feather
+                  name="clock"
+                  size={15}
+                  color="#ADD8E6"
+                  style={{ alignSelf: "center" }}
+                />
+                <Text>
+                  {modalProps?.startTime}-{modalProps?.endTime}
+                </Text>
+                <Text>{modalProps?.date}</Text>
+              </View>
+              <View style={styles.modalRow}>
+                <Feather
+                  name="map-pin"
+                  size={15}
+                  color="#ADD8E6"
+                  style={{ alignSelf: "center" }}
+                />
+                <Text>{modalProps?.location}</Text>
+              </View>
+              <View style={styles.modalRow}>
+                <Feather
+                  name="book-open"
+                  size={15}
+                  color="#ADD8E6"
+                  style={{ alignSelf: "center" }}
+                />
+                <WebLink
+                  url={String(modalProps?.ufindLink)}
+                  name="u:find course overview"
+                />
+              </View>
+              <View style={styles.modalRow}>
+                <MoodleLogo
+                  width={20}
+                  height={20}
+                  fill={"#ADD8E6"}
+                  style={{ alignSelf: "center", marginRight: -5 }}
+                />
+                <WebLink
+                  url={String(modalProps?.moodleLink)}
+                  name="Moodle course material"
+                />
+              </View>
+            </Pressable>
           </Pressable>
         </Modal>
       </SafeAreaView>
 
-      <StatusBar style='dark' />
+      <StatusBar style="dark" />
     </>
   );
 }
 
 function generateModalProps(event: PackedEvent): CalendarModalProps {
   const props: CalendarModalProps = {
-    title: event.title ? event.title : 'unknown',
-    date: 'unknown',
-    startTime: 'unknown',
-    endTime: 'unknown',
-    location: event.location ? event.location : 'unknown',
-  }
+    title: event.title ? event.title : "unknown",
+    date: "unknown",
+    startTime: "unknown",
+    endTime: "unknown",
+    location: event.location,
+    ufindLink: event.ufindLink,
+    moodleLink: event.moodleLink,
+  };
 
   if (event.startHour && event.duration) {
     const startHours: number = Math.floor(event.startHour);
-    const startMinutes: number = Math.round((event.startHour - startHours) * 60);
+    const startMinutes: number = Math.round(
+      (event.startHour - startHours) * 60
+    );
     const endHours: number = Math.floor(event.startHour + event.duration);
-    const endMinutes: number = Math.round((event.startHour + event.duration - endHours) * 60);
+    const endMinutes: number = Math.round(
+      (event.startHour + event.duration - endHours) * 60
+    );
 
     const start = new Date();
     const end = new Date();
@@ -102,40 +169,53 @@ function generateModalProps(event: PackedEvent): CalendarModalProps {
     start.setHours(startHours, startMinutes);
     end.setHours(endHours, endMinutes);
 
-    props.startTime = start.toTimeString().slice(0,5);
+    props.startTime = start.toTimeString().slice(0, 5);
     props.endTime = end.toTimeString().slice(0, 5);
-    
+
     const date = new Date(Date.parse(event.start));
-    props.date = date.toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) 
+    props.date = date.toLocaleDateString("en-us", {
+      weekday: "long",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    });
   }
 
   return props;
 }
 
 async function fetchCourses(session: string): Promise<EventItem[]> {
-  let courses: CourseData[] = []
+  let courses: CourseData[] = [];
   let events: EventItem[] = [];
   let id: number = 1;
-  
+
   const uClient = new UspaceClient();
   uClient.setSession = session;
   courses = await (await uClient.getCourses(2024, false)).json();
-  
+
   const uniqueCourses = new Set();
   for (const course of courses) {
-    
     const lvNr: string = course.lehrveranstaltung.lvNr;
     if (uniqueCourses.has(lvNr) || course.status === "ABGEMELDET") continue;
     else uniqueCourses.add(lvNr);
 
     const title: string = course.lehrveranstaltung.titel;
-    const color: string = '#ADD8E6';
+    const color: string = "#ADD8E6";
+    const moodleLink: string = course.elearningCourse
+      ? course.elearningCourse.url
+      : "https://moodle.unievie.ac.at/";
 
     const uniqueDates = new Set();
     for (const date of course.lehrveranstaltung.termine) {
-      const start: string = (new Date(date.beginn)).toISOString();
-      const end: string = (new Date(date.ende)).toISOString();
+      const start: string = new Date(date.beginn).toISOString();
+      const end: string = new Date(date.ende).toISOString();
       const location: string = date.raumName;
+
+      const year: number = new Date(Date.parse(start)).getFullYear();
+      const month: number = new Date(Date.parse(start)).getMonth();
+      const semester: "W" | "S" = month < 2 || month > 7 ? "W" : "S";
+
+      const ufindLink: string = `https://ufind.univie.ac.at/en/course.html?lv=${lvNr}&semester=${year}${semester}`;
 
       if (uniqueDates.has(start)) continue;
       else uniqueDates.add(start);
@@ -147,7 +227,9 @@ async function fetchCourses(session: string): Promise<EventItem[]> {
         start: start,
         end: end,
         location: location,
-      }
+        ufindLink: ufindLink,
+        moodleLink: moodleLink,
+      };
 
       events.push(newEvent);
       id++;
@@ -157,23 +239,22 @@ async function fetchCourses(session: string): Promise<EventItem[]> {
   return events;
 }
 
-
 const styles = StyleSheet.create({
-  container: { 
-    flex: 1, 
-    backgroundColor: '#FFF',
-   },
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+  },
   modalContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   modalView: {
     width: ScreenWidth * 0.9,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     borderRadius: 5,
     padding: 10,
-    shadowColor: '#000',
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -183,20 +264,22 @@ const styles = StyleSheet.create({
     elevation: 5,
   },
   modalRow: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 10,
     marginBottom: 15,
-    width: ScreenWidth * 0.9 - 40
+    width: ScreenWidth * 0.9 - 40,
   },
   modalTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 5,
   },
   divider: {
-    borderBottomColor: 'black',
+    borderBottomColor: "black",
     borderBottomWidth: 1.5,
     marginBottom: 15,
   },
+  link: {
+    color: "#25A9E2",
+  },
 });
-
