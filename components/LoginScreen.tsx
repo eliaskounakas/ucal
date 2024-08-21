5b4b8ebf4d7c96a7e8f09b6b1ef6120c782a8c67
Elias Kounakas <eliaskounakas@proton.me>
2024-08-21 13:14:50 +0200

    change tsc version and add error handling for failed auto login

--- components/LoginScreen.tsx
+++ components/LoginScreen.tsx
@@ -51,6 +51,7 @@ export default function Login({ setSession }: LoginProps) {
       })
       .finally(() => {
         SplashScreen.hideAsync();
+        setIsLoading(false);
       });
   }, []);
 
